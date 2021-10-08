c82ad19f9f739153287c1f9aa056b3f4b8abbbc0
John Irle <irlejohn@gmail.com>
2021-10-08 15:48:14 -0500

    add not-found route catch and error

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express from "express";
 import "express-async-errors";
 import { whoisRouter } from "./routes/whois";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(express.json());
 
 app.use(whoisRouter);
 
+app.all("*", async (req, res) => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
+
 export { app };
