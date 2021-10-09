398d1f1136b724837cc236da12ccb5197a2f607e
John Irle <irlejohn@gmail.com>
2021-10-09 06:47:18 -0500

    key test

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import { app } from "./app";
 const PORT = 80;
 
 const start = async () => {
+    if (process.env.RAPID_API_KEY) {
+        console.log("R KEY");
+    }
+    if (process.env.WHOIS_API_KEY) {
+        console.log("W KEY");
+    }
     app.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
     });
