94955422697d306d912ec7a34595d00cc614a518
John Irle <irlejohn@gmail.com>
2021-10-09 09:49:20 -0500

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "express-async-errors";
 import { whoisRouter } from "./routes/whois";
 import { errorHandler } from "./middleware/error-handler";
@@ -6,8 +7,18 @@ import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(whoisRouter);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 app.all("*", async (req, res) => {
     throw new NotFoundError();
